f7199af950e5868f3ddf055dfa08ffc49990c53d
HeeJeongSeo <heejung9965@naver.com>
2024-12-16 23:10:30 +0900

    날짜 입력 이동 & 오늘 날짜 이동 기능 추가

--- script.js
+++ script.js
@@ -1,8 +1,11 @@
-const calendar = document.querySelector(".calendar");
-    date = document.querySelector(".date");
-    daysContainer = document.querySelector(".days");
-    prev = document.querySelector(".prev");
-    next = document.querySelector(".next");
+const calendar = document.querySelector(".calendar"),
+    date = document.querySelector(".date"),
+    daysContainer = document.querySelector(".days"),
+    prev = document.querySelector(".prev"),
+    next = document.querySelector(".next"),
+    todayBtn = document.querySelector(".today-btn"),
+    gotoBtn = document.querySelector(".goto-btn"),
+    dateInput = document.querySelector(".date-input");
 
 let today = new Date();
 let activeDay;
@@ -91,3 +94,51 @@ function nextMonth(){
 // 이전 다음 달 넘기는 클릭 기능
 prev.addEventListener("click", prevMonth);
 next.addEventListener("click", nextMonth);
+
+
+
+
+// 날짜 선택 이동 &  오늘 날짜 이동 기능 추가
+todayBtn.addEventListener("click", ()=> {
+    today = new Date();
+    month  = today.getMonth();
+    year = today.getFullYear();
+    initCalendar();
+});
+
+dateInput.addEventListener("input", (e) => {
+    //숫자와 슬래시를 제외한 모든 문자 제거
+    dateInput.value = dateInput.value.replace(/[^0-9/]/g, "");
+    // 숫자 두 개를 적으면 슬래시 붙이기
+    if(dateInput.value.length === 2) {
+        dateInput.value += "/";
+    }
+    // 숫자 길이가 7개가 넘지 않도록 하기
+    if(dateInput.value.length > 7) {
+        dateInput.value = dateInput.value.slice(0,7);
+    }
+    // backspace를 누를경우
+    if(e.inputType === "deleteContentBackward") {
+        if(dateInput.value.length === 3) {
+            dateInput.value = dateInput.value.slice(0,2);
+        }
+    }
+});
+
+gotoBtn.addEventListener("click",gotoDate);
+
+// 입력된 날짜로 가는 기능
+function gotoDate() {
+    const dateArr = dateInput.value.split("/");
+    // 입력 날짜 정확성 확인
+    if(dateArr.length === 2) {
+        if(dateArr[0] > 0 && dateArr[0] < 13 && dateArr[1].length === 4) {
+            month = dateArr[0] - 1;
+            year = dateArr[1];
+            initCalendar();
+            return;
+        }
+    }
+    // 입력한 날짜가 틀리다면
+    alert("invalid date");
+}
